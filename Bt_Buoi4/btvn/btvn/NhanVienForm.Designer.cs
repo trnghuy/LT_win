0ebb16798a525d87e99162586cd076d00faf91e0
Trần Ngọc Huy <128377575+trnghuy@users.noreply.github.com>
2024-12-01 15:27:45 +0700

    Add files via upload

---
+++ Bt_Buoi4/btvn/btvn/NhanVienForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace btvn
+{
+    partial class NhanVienForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDongY = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLuongCB = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLuongCB = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDongY
+            // 
+            this.btnDongY.Location = new System.Drawing.Point(159, 310);
+            this.btnDongY.Name = "btnDongY";
+            this.btnDongY.Size = new System.Drawing.Size(129, 60);
+            this.btnDongY.TabIndex = 0;
+            this.btnDongY.Text = "Đồng ý";
+            this.btnDongY.UseVisualStyleBackColor = true;
+            this.btnDongY.Click += new System.EventHandler(this.btnDongY_Click);
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(0, 0);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(75, 23);
+            this.btnSkip.TabIndex = 8;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(83, 88);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(74, 13);
+            this.labelID.TabIndex = 2;
+            this.labelID.Text = "Ma Nhan vien";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(83, 157);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(76, 13);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Ten nhan vien";
+            // 
+            // labelLuongCB
+            // 
+            this.labelLuongCB.AutoSize = true;
+            this.labelLuongCB.Location = new System.Drawing.Point(83, 239);
+            this.labelLuongCB.Name = "labelLuongCB";
+            this.labelLuongCB.Size = new System.Drawing.Size(73, 13);
+            this.labelLuongCB.TabIndex = 4;
+            this.labelLuongCB.Text = "Luong co ban";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(238, 88);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(236, 20);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(238, 157);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(236, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtLuongCB
+            // 
+            this.txtLuongCB.Location = new System.Drawing.Point(238, 232);
+            this.txtLuongCB.Name = "txtLuongCB";
+            this.txtLuongCB.Size = new System.Drawing.Size(236, 20);
+            this.txtLuongCB.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(442, 310);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 60);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NhanVienForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtLuongCB);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.labelLuongCB);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.btnSkip);
+            this.Controls.Add(this.btnDongY);
+            this.Name = "NhanVienForm";
+            this.Text = "NhanVienForm";
+            this.Load += new System.EventHandler(this.NhanVienForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDongY;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLuongCB;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLuongCB;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
