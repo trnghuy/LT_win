8bf22e53796a98d26cc14005673e52d10aafce88
Trần Ngọc Huy <128377575+trnghuy@users.noreply.github.com>
2024-12-19 00:15:25 +0700

    Add files via upload

---
+++ btvn6/src/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitapbuoi6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var context = new Model1();
+            List<Student> list = context.Students.ToList();
+            dataGridView1.DataSource = list;
+
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+    }
+}
---
+++ btvn6/src/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitapbuoi6._1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var context = new Model1();
+            List<Faculty> list = context.Faculties.ToList();
+            dataGridView1.DataSource = list;
+        }
+    }
+}
